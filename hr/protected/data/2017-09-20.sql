612cd44da3391afa8ceecaaeffad4c3838e33f08
沈超 <shenchao@lbsgroup.com.cn>
2017-09-20 15:28:56 +0800

    公司數據表及職位表的修改

--- hr/protected/data/2017-09-20.sql
+++ hr/protected/data/2017-09-20.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50620
 File Encoding         : 65001
 
-Date: 2017-09-20 14:56:47
+Date: 2017-09-20 15:28:16
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,7 +27,7 @@ CREATE TABLE `hr_company` (
   `address` varchar(255) NOT NULL COMMENT '公司地址',
   `city` varchar(30) NOT NULL COMMENT '公司歸屬地區',
   `phone` varchar(255) DEFAULT NULL COMMENT '公司電話',
-  `tacitly` int(11) DEFAULT '0' COMMENT '默認公司：0（否）1（是）',
+  `tacitly` varchar(11) DEFAULT '0' COMMENT '默認公司：0（否）1（是）',
   `organization_code` varchar(30) DEFAULT NULL COMMENT '組織機構代碼',
   `organization_time` date DEFAULT NULL COMMENT '組織機構代碼發出時間',
   `security_code` varchar(30) DEFAULT NULL COMMENT '勞動保障代碼',
@@ -38,7 +38,7 @@ CREATE TABLE `hr_company` (
   `lcd` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `lud` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='公司資料表';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='公司資料表';
 
 -- ----------------------------
 -- Table structure for hr_dept
@@ -47,7 +47,7 @@ DROP TABLE IF EXISTS `hr_dept`;
 CREATE TABLE `hr_dept` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `z_index` int(11) DEFAULT '0',
+  `z_index` varchar(11) DEFAULT '0',
   `type` int(11) NOT NULL DEFAULT '0' COMMENT '0:部門  1:職位',
   `city` varchar(255) DEFAULT NULL,
   `dept_id` int(11) DEFAULT '1' COMMENT '部門id',
@@ -56,4 +56,4 @@ CREATE TABLE `hr_dept` (
   `lcd` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `lud` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='工作部門';
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COMMENT='工作部門';
