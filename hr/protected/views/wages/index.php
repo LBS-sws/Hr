067565a30a4907946c118610a94080dd4647141b
沈超 <shenchao@lbsgroup.com.cn>
2017-10-19 17:50:59 +0800

    權限修改

--- hr/protected/views/wages/index.php
+++ hr/protected/views/wages/index.php
@@ -27,7 +27,7 @@ $this->pageTitle=Yii::app()->name . ' - Wages';
             <div class="btn-group" role="group">
                 <?php
                 //var_dump(Yii::app()->session['rw_func']);
-                if (Yii::app()->user->validRWFunction('ZD01'))
+                if (Yii::app()->user->validRWFunction('ZA03'))
                     echo TbHtml::button('<span class="fa fa-file-o"></span> '.Yii::t('contract','Add Wages Type'), array(
                         'submit'=>Yii::app()->createUrl('wages/new'),
                     ));
