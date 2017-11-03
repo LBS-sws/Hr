4d79f67771be719dd451a56341792b05ad94ba1b
LBS IT <percylee@lbsgroup.com.hk>
2017-11-03 16:42:15 +0800

    Percy - 調整合同字型

--- hr/protected/components/Template.php
+++ hr/protected/components/Template.php
@@ -116,17 +116,20 @@ class Template {
     }
 
     public function resetContractWord($testBool,$contractBool){
-        $str ='</w:t></w:r><w:r><w:rPr><w:rFonts w:ascii="DFKai-SB" w:hAnsi="DFKai-SB" w:cs="Arial" w:hint="eastAsia"/><w:kern w:val="0"/><w:szCs w:val="18"/><w:u w:val="single"/><w:lang w:eastAsia="zh-CN"/></w:rPr><w:t>';
-        $str2 ='</w:t></w:r><w:r><w:rPr><w:rFonts w:ascii="DFKai-SB" w:hAnsi="DFKai-SB" w:cs="Arial" w:hint="eastAsia"/><w:kern w:val="0"/><w:szCs w:val="18"/><w:lang w:eastAsia="zh-CN"/></w:rPr><w:t>';
+        $str ='</w:t></w:r><w:r><w:rPr><w:rFonts w:ascii="FangSong" w:hAnsi="FangSong" w:cs="FangSong" w:hint="eastAsia"/><w:kern w:val="0"/><w:szCs w:val="18"/><w:u w:val="single"/><w:lang w:eastAsia="zh-CN"/></w:rPr><w:t>';
+        $str2 ='</w:t></w:r><w:r><w:rPr><w:rFonts w:ascii="FangSong" w:hAnsi="FangSong" w:cs="FangSong" w:hint="eastAsia"/><w:kern w:val="0"/><w:szCs w:val="18"/><w:lang w:eastAsia="zh-CN"/></w:rPr><w:t>';
         //合同期限
         if($contractBool){
-            $this->setValue("contractdeadline","有固定期限：从".$str."staffyears1".$str2."年".$str."staffmonth1".$str2."月".$str."staffday1".$str2."日起至".$str."staffyears2".$str2."年".$str."staffmonth2".$str2."月".$str."staffday2".$str2."日止。");
+//            $this->setValue("contractdeadline","有固定期限：从".$str."staffyears1".$str2."年".$str."staffmonth1".$str2."月".$str."staffday1".$str2."日起至".$str."staffyears2".$str2."年".$str."staffmonth2".$str2."月".$str."staffday2".$str2."日止。");
+            $this->setValue("contractdeadline","有固定期限：从staffyears1年staffmonth1月staffday1日起至staffyears2年staffmonth2月staffday2日止。");
         }else{
-            $this->setValue("contractdeadline","无固定期限：从".$str."staffyears3".$str2."年".$str."staffmonth3".$str2."月".$str."staffday3".$str2."日起至法定的终止条件出现时止。");
+//            $this->setValue("contractdeadline","无固定期限：从".$str."staffyears3".$str2."年".$str."staffmonth3".$str2."月".$str."staffday3".$str2."日起至法定的终止条件出现时止。");
+            $this->setValue("contractdeadline","无固定期限：从staffyears3年staffmonth3月staffday3日起至法定的终止条件出现时止。");
         }
         //試用期
         if($testBool){
-            $this->setValue("testdeadline","试用期为".$str."stafftest".$str2."个月，从".$str."stafftestyears1".$str2."年".$str."stafftestmonth1".$str2."月".$str."stafftestday1".$str2."日起至".$str."stafftestyears2".$str2."年".$str."stafftestmonth2".$str2."月".$str."stafftestday2".$str2."日止，试用期工资为RMB ".$str."stafftestwage".$str2." 元/月。");
+//            $this->setValue("testdeadline","试用期为".$str."stafftest".$str2."个月，从".$str."stafftestyears1".$str2."年".$str."stafftestmonth1".$str2."月".$str."stafftestday1".$str2."日起至".$str."stafftestyears2".$str2."年".$str."stafftestmonth2".$str2."月".$str."stafftestday2".$str2."日止，试用期工资为RMB ".$str."stafftestwage".$str2." 元/月。");
+            $this->setValue("testdeadline","试用期为stafftest个月，从stafftestyears1年stafftestmonth1月stafftestday1日起至stafftestyears2年stafftestmonth2月stafftestday2日止，试用期工资为RMB stafftestwage 元/月。");
         }else{
             $this->setValue("testdeadline","无试用期。");
         }
