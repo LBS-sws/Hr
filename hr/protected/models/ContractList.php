04ce77675475db0caf4715b59441f081fa4fd1be
沈超 <shenchao@lbsgroup.com.cn>
2017-11-15 09:09:49 +0800

    合同模板權限的修改

--- hr/protected/models/ContractList.php
+++ hr/protected/models/ContractList.php
@@ -27,6 +27,11 @@ class ContractList extends CListPageModel
 				from hr_contract 
 				where id!=0  
 			";
+        $rw = Yii::app()->user->validRWFunction("ZD01");
+        if(!$rw){
+            $sql1.=" and city='$city' ";
+            $sql2.=" and city='$city' ";
+        }
 		$clause = "";
 		if (!empty($this->searchField) && !empty($this->searchValue)) {
 			$svalue = str_replace("'","\'",$this->searchValue);
