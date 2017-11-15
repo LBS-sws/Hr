5d0205164f10858d6a2a2959e22d321320dbf82d
沈超 <shenchao@lbsgroup.com.cn>
2017-11-15 10:07:12 +0800

    合同模板的權限

--- hr/protected/views/contract/index.php
+++ hr/protected/views/contract/index.php
@@ -27,7 +27,7 @@ $this->pageTitle=Yii::app()->name . ' - Contract';
             <div class="btn-group" role="group">
                 <?php
                 //var_dump(Yii::app()->session['rw_func']);
-                if (Yii::app()->user->validRWFunction('ZD01'))
+                if (Yii::app()->user->validRWFunction('ZD02'))
                     echo TbHtml::button('<span class="fa fa-file-o"></span> '.Yii::t('contract','Add Contract'), array(
                         'submit'=>Yii::app()->createUrl('contract/new'),
                     ));
