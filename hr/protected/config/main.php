d0b4fbeba29123836a96c4a292c63a7e5d5e17e9
LBS IT <percylee@lbsgroup.com.hk>
2019-09-23 09:38:14 +0800

    設定檔main.php修正

--- hr/protected/config/main.php
+++ hr/protected/config/main.php
@@ -128,7 +128,7 @@ return array(
 		'docmanPath'=>'/docman/dev',
 		'systemId'=>'hr',
 		'envSuffix'=>'dev',
-		'employeeCode'='4',
+		'employeeCode'=>'4',
 		'appname'=>'LBS DMS (UAT)',
 		'appcolor'=>'skin-red-light',
 	),
