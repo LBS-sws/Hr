90753f7193018d115e0efcbad5b112b9c7cec0a5
shenchao <shenchao@lbsgroup.com.cn>
2023-03-30 17:04:13 +0800

    顯示當年入職的銷售人員

--- hr/protected/models/BossReview.php
+++ hr/protected/models/BossReview.php
@@ -847,6 +847,7 @@ class BossReview
                     }
                 }
                 if(intval($entry_time)==$year){ //入職或者變更等於老總年度考核的時間
+                    echo "<div class='hide' data-sales='sales-user'>user_id:{$row["user_id"]},employee_id:{$row["id"]}</div>";
                     $count++;
                     /*  查询月份不影响销售5步曲分数*/
                     $datetime = date("Y/m/d",strtotime($entry_time." + 2 month"));
