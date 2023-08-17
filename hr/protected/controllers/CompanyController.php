fc7a85d59f698f964c3a129b0e1a0f23b18e6926
xiangsong <xiangsong.cd@lbsgroup.com.cn>
2023-08-17 10:45:08 +0800

    测试员工附件问题

--- hr/protected/controllers/CompanyController.php
+++ hr/protected/controllers/CompanyController.php
@@ -29,11 +29,11 @@ class CompanyController extends Controller
     {
         return array(
             array('allow',
-                'actions'=>array('new','edit','save','delete','fileupload','fileRemove'),
+                'actions'=>array('new','edit','save','delete','fileupload','fileRemove','test'),
                 'expression'=>array('CompanyController','allowReadWrite'),
             ),
             array('allow',
-                'actions'=>array('index','view','fileDownload'),
+                'actions'=>array('index','view','fileDownload','test'),
                 'expression'=>array('CompanyController','allowReadOnly'),
             ),
             array('deny',  // deny all users
@@ -49,6 +49,22 @@ class CompanyController extends Controller
         return Yii::app()->user->validFunction('ZA02');
     }
 
+    public function actionTest(){
+
+// 调用方法并传递静态资源路径
+        $resourcePath = '/data/docman/upload/sal/uat/57/0/4731ca7a290485a311e4e05f8d55ff3f.jpg';
+        $resourceContent = $this->readStaticResource($resourcePath);
+
+// 处理文件内容，例如输出到控制台
+        var_dump($resourceContent);
+    }
+
+    public function readStaticResource($resourcePath) {
+        // 读取静态资源文件内容
+        $content = file_get_contents($resourcePath);
+        // 返回文件内容
+        return $content;
+    }
     public function actionIndex($pageNum=0){
         $model = new CompanyList;
         if (isset($_POST['CompanyList'])) {
